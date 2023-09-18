0991804e59a88277d821ef698e23694507be8568
Crstt <matteo598.catalano@gmail.com>
2023-09-18 18:02:13 -0400

    Starting to work on table and subtotals

--- SDEV240GroupProject/Form1.Designer.cs
+++ SDEV240GroupProject/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridView1 = new DataGridView();
+            Category = new DataGridViewTextBoxColumn();
+            Item = new DataGridViewTextBoxColumn();
+            Material = new DataGridViewTextBoxColumn();
+            Size_Desc = new DataGridViewTextBoxColumn();
+            Quantity = new DataGridViewTextBoxColumn();
+            Cost = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Category, Item, Material, Size_Desc, Quantity, Cost });
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(967, 834);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Category
+            // 
+            Category.HeaderText = "Category";
+            Category.MinimumWidth = 8;
+            Category.Name = "Category";
+            Category.Width = 150;
+            // 
+            // Item
+            // 
+            Item.HeaderText = "Item";
+            Item.MinimumWidth = 8;
+            Item.Name = "Item";
+            Item.Width = 150;
+            // 
+            // Material
+            // 
+            Material.HeaderText = "Material";
+            Material.MinimumWidth = 8;
+            Material.Name = "Material";
+            Material.Width = 150;
+            // 
+            // Size_Desc
+            // 
+            Size_Desc.HeaderText = "Size / Description";
+            Size_Desc.MinimumWidth = 8;
+            Size_Desc.Name = "Size_Desc";
+            Size_Desc.Width = 150;
+            // 
+            // Quantity
+            // 
+            Quantity.HeaderText = "Quantity";
+            Quantity.MinimumWidth = 8;
+            Quantity.Name = "Quantity";
+            Quantity.Width = 150;
+            // 
+            // Cost
+            // 
+            Cost.HeaderText = "Cost";
+            Cost.MinimumWidth = 8;
+            Cost.Name = "Cost";
+            Cost.Width = 150;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(162, 909);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1090, 1074);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Category;
+        private DataGridViewTextBoxColumn Item;
+        private DataGridViewTextBoxColumn Material;
+        private DataGridViewTextBoxColumn Size_Desc;
+        private DataGridViewTextBoxColumn Quantity;
+        private DataGridViewTextBoxColumn Cost;
+        private Button button1;
     }
 }
--- SDEV240GroupProject/Form1.cs
+++ SDEV240GroupProject/Form1.cs
@@ -1,10 +1,52 @@
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+
 namespace SDEV240GroupProject
 {
     public partial class Form1 : Form
     {
+        List<string> columnNames = new List<string>();
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
+
+            // Iterate through the DataGridView's columns
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                columnNames.Add(column.HeaderText); // Use .Name if you have explicitly set column names
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Sort the DataGridView by the Category column
+            dataGridView1.Sort(Category, ListSortDirection.Ascending);
+            UpdateSubtotalRows();
+        }
+
+        private void UpdateSubtotalRows()
+        {
+            string currentCategory = null;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                string category = (string)dataGridView1.Rows[i].Cells[Category.Index].Value;
+
+                if (category != currentCategory && category != "Subtotal")
+                {
+                    // Insert a subtotal row before the current row
+                    dataGridView1.Rows.Insert(i, "Subtotal", "", "", "", "", "");
+
+                    // Update the current category
+                    currentCategory = category;
+
+                    // Skip to the next row
+                    i++;
+                }
+            }
         }
     }
 }
