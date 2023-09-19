271858e728309e710340b3ecee14b9dd5f55fcfd
Crstt <matteo598.catalano@gmail.com>
2023-09-19 14:18:43 -0400

    Adds basic sorting and subtotals

--- SDEV240GroupProject/Form1.Designer.cs
+++ SDEV240GroupProject/Form1.Designer.cs
@@ -28,27 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             dataGridView1 = new DataGridView();
             Category = new DataGridViewTextBoxColumn();
             Item = new DataGridViewTextBoxColumn();
             Material = new DataGridViewTextBoxColumn();
-            Size_Desc = new DataGridViewTextBoxColumn();
+            SizeDesc = new DataGridViewTextBoxColumn();
             Quantity = new DataGridViewTextBoxColumn();
             Cost = new DataGridViewTextBoxColumn();
             button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Category, Item, Material, Size_Desc, Quantity, Cost });
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Category, Item, Material, SizeDesc, Quantity, Cost });
+            dataGridView1.Location = new Point(8, 7);
+            dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.RowTemplate.Height = 33;
-            dataGridView1.Size = new Size(967, 834);
+            dataGridView1.Size = new Size(965, 500);
             dataGridView1.TabIndex = 0;
+            dataGridView1.RowValidated += dataGridView1_RowValidated;
+            dataGridView1.RowValidating += dataGridView1_RowValidating;
+            dataGridView1.UserDeletingRow += dataGridView1_UserDeletingRow;
             // 
             // Category
             // 
@@ -71,12 +77,12 @@
             Material.Name = "Material";
             Material.Width = 150;
             // 
-            // Size_Desc
+            // SizeDesc
             // 
-            Size_Desc.HeaderText = "Size / Description";
-            Size_Desc.MinimumWidth = 8;
-            Size_Desc.Name = "Size_Desc";
-            Size_Desc.Width = 150;
+            SizeDesc.HeaderText = "Size / Description";
+            SizeDesc.MinimumWidth = 8;
+            SizeDesc.Name = "SizeDesc";
+            SizeDesc.Width = 150;
             // 
             // Quantity
             // 
@@ -94,21 +100,27 @@
             // 
             // button1
             // 
-            button1.Location = new Point(162, 909);
+            button1.Location = new Point(8, 544);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(78, 20);
             button1.TabIndex = 1;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1090, 1074);
+            ClientSize = new Size(985, 713);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -118,12 +130,13 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
         private DataGridViewTextBoxColumn Category;
         private DataGridViewTextBoxColumn Item;
         private DataGridViewTextBoxColumn Material;
-        private DataGridViewTextBoxColumn Size_Desc;
+        private DataGridViewTextBoxColumn SizeDesc;
         private DataGridViewTextBoxColumn Quantity;
         private DataGridViewTextBoxColumn Cost;
-        private Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SDEV240GroupProject/Form1.cs
+++ SDEV240GroupProject/Form1.cs
@@ -8,16 +8,18 @@ namespace SDEV240GroupProject
     public partial class Form1 : Form
     {
         List<string> columnNames = new List<string>();
+        List<int> subtotalRows = new List<int>();
+        private bool validationDone = false;
+
         public Form1()
         {
-            InitializeComponent();           
+            InitializeComponent();
 
             // Iterate through the DataGridView's columns
             foreach (DataGridViewColumn column in dataGridView1.Columns)
             {
                 columnNames.Add(column.HeaderText); // Use .Name if you have explicitly set column names
             }
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,22 +32,99 @@ namespace SDEV240GroupProject
         private void UpdateSubtotalRows()
         {
             string currentCategory = null;
+            subtotalRows = new List<int>();
 
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
                 string category = (string)dataGridView1.Rows[i].Cells[Category.Index].Value;
 
-                if (category != currentCategory && category != "Subtotal")
+                if (category != null)
                 {
-                    // Insert a subtotal row before the current row
-                    dataGridView1.Rows.Insert(i, "Subtotal", "", "", "", "", "");
+                    if (category != currentCategory)
+                    {
+                        //for (int k = i; k < dataGridView1.Rows.Count; k++)
+                        int k = i;
+                        string nextCategory;
+                        do
+                        {
+                            k++;
+                            nextCategory = (string)dataGridView1.Rows[k].Cells[Category.Index].Value;
+                        } while (nextCategory != null && (nextCategory == category));
 
-                    // Update the current category
-                    currentCategory = category;
+                        if (nextCategory == null || !nextCategory.Contains("Subtotal"))
+                        {
+                            // Insert a subtotal row before the current row
+                            dataGridView1.Rows.Insert(k, $"{category} - Subtotal", "", "", "", "", "");
+                            dataGridView1.Rows[k].ReadOnly = true;
+                            subtotalRows.Add(k);
 
-                    // Skip to the next row
-                    i++;
+                            // Update the current category
+                            currentCategory = category;
+                        }
+                        // Skip to the next row
+                        i = k++;
+                    }
                 }
+            }
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            // Check if you want to prevent deletion of a specific row
+            if (subtotalRows.IndexOf(e.Row.Index) != -1)
+            {
+                e.Cancel = true; // Cancel row deletion
+            }
+        }
+
+        private void dataGridView1_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            if (!validationDone)
+            {
+                // Check if any of the required cells are empty (modify the column indices as needed)
+                if (string.IsNullOrWhiteSpace(row.Cells[Category.Index].Value?.ToString()) ||
+                    string.IsNullOrWhiteSpace(row.Cells[Item.Index].Value?.ToString()) ||
+                    string.IsNullOrWhiteSpace(row.Cells[Material.Index].Value?.ToString()) ||
+                    string.IsNullOrWhiteSpace(row.Cells[SizeDesc.Index].Value?.ToString()) ||
+                    string.IsNullOrWhiteSpace(row.Cells[Quantity.Index].Value?.ToString()) ||
+                    string.IsNullOrWhiteSpace(row.Cells[Cost.Index].Value?.ToString()))
+                {
+                    // Display an error message (you can use a MessageBox or any other method)
+                    MessageBox.Show("Please fill in all required cells before moving to the next row.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    // Cancel row validation to prevent moving to the next row
+                    e.Cancel = true;
+                }
+                else
+                {
+                    // Set a flag to indicate validation failure
+                    validationDone = true;
+
+                    // Enable the timer to perform sorting after a short delay
+                    timer1.Enabled = true;
+                }
+            }            
+        }
+
+        private void dataGridView1_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Check if validation done and sorting is pending
+            if (validationDone)
+            {
+                // Sort the DataGridView by the Category column
+                dataGridView1.Sort(Category, ListSortDirection.Ascending);
+                UpdateSubtotalRows();
+                // Reset the flag
+                validationDone = false;
+
+                // Disable the timer
+                timer1.Enabled = false;
             }
         }
     }
